dc1d9c1d7aa31a1cd7dbfc9fb4a2bd067e523bdc
Halil24 <halilbagosi24@icloud.com>
2024-11-10 10:18:55 +0100

    Author.java

--- src/Ex3/Author.java
+++ src/Ex3/Author.java
@@ -1,4 +1,24 @@
 package Ex3;
 
 public class Author {
+    private String name;
+    private String surname;
+    Author(String n, String sn) {
+        name = n;
+        surname = sn;
+    }
+public String getFirstName() {
+        return name;
+}
+public String getLastName() {
+        return surname;
+}
+
+public void setName(String nm) {
+        name=nm;
+    }
+    public void setLastName(String snm) {
+        surname=snm;
+    }
+
 }
